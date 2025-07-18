4fa6a4a6303bf44153667e27ef30e8573c68e64e
V-Gulev <velizargulev@abv.bg>
2025-07-18 22:07:33 +0300

    Json Exercise

---
+++ src/main/java/org/example/jsonexercise/JsonExerciseApplication.java
@@ -0,0 +1,14 @@
+package org.example.jsonexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonExerciseApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/example/jsonexercise/JsonExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jsonexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
